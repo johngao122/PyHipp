c90e60046ac84da35b4aed7376749557db78d7c2
AWS ParallelCluster user <ec2-user@ip-10-0-0-14.ap-southeast-1.compute.internal>
2025-09-12 15:56:28 +0000

    fix slurm files

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1   # number of CPUs for this task
-#SBATCH -J "rpllfp"   # job name
+#SBATCH -J "freq"   # job name
 
 ## /SBATCH -p general # partition (queue)
 #SBATCH -o rpllfp-slurm.%N.%j.out # STDOUT
--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -15,10 +15,11 @@
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
 python -u -c "import DataProcessingTools as DPT; \
+import PyHipp as pyh; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
+hfall.save();"
 
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:253490772629:awsnotify --message "FSJobDone"
